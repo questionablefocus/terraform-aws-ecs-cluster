c8e0d443df8c11ca02a5f66b7d42d6b27f2b5214
Marcus Mo <marcus.mo@questionablefocus.com>
2025-06-02 16:00:41 +0100

    Initial commit

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "cluster_id" {
+  description = "ID of the ECS cluster"
+  value       = aws_ecs_cluster.main.id
+}
+
+output "task_execution_role_arn" {
+  description = "ARN of the ECS task execution role"
+  value       = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "task_role_arn" {
+  description = "ARN of the ECS task role"
+  value       = aws_iam_role.ecs_task_role.arn
+}

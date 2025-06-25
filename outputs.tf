32086471e45870d6f9d6bf82c5668c10ccb7d285
Marcus Mo <marcus.mo@questionablefocus.com>
2025-06-25 14:31:17 +0100

    Output cluster name

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "cluster_id" {
   value       = aws_ecs_cluster.main.id
 }
 
+output "cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
+
 output "task_execution_role_arn" {
   description = "ARN of the ECS task execution role"
   value       = aws_iam_role.ecs_task_execution_role.arn
